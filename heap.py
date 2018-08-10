2a1f7146502eacd1dce6b22419afe09d3c20896f
fabrezi <fkschon@gmail.com>
2018-08-10 17:02:03 -0400

    Add files via upload

---
+++ heap.py
@@ -0,0 +1,14 @@
+#implement bubble sort
+
+def bubble(list):
+    for passnum in range(len(list)-1,0,-1):#range(start,stop,step)
+        for i in range(passnum):
+            if list[i]>list[i+1]:
+                temp = list[i]
+                list[i] = list[i+1]
+                list[i+1] = temp
+
+list = [54,16,99,75,3]
+bubble(list)
+print(list)
+
