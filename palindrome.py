066d04212573f4b8f159a33d6e6acac8f38c6540
fabrezi <fkschon@gmail.com>
2018-08-10 13:41:45 -0400

    Create palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,44 @@
+#solve palindrome problem:
+#feedback: requires user input
+
+class stack():
+    def __init__(self):
+        self.item = []
+
+    def push(self, key):
+        self.item.append(key)
+
+    def pop(self):
+        return self.item.pop()
+
+class queue():
+    def __init__(self):
+        self.item = []
+
+    def push(self, key):
+        self.item.append(key)
+
+    def pop(self):
+        return self.item.pop(0)
+
+class checker():
+    def __init__(self, word):
+        self.word = word
+        self.queue = queue()
+        self.stack = stack()
+    def checker(self):
+        for letter in self.word:
+            self.queue.push(letter)
+            self.stack.push(letter)
+        for i in range(len(self.word)):
+            if self.stack.pop() != self.queue.pop():
+                return False
+            else:
+                return True
+
+palindrome_checker = checker('MAM')
+print (palindrome_checker.checker())
+
+
+
+
