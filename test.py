155b63724eef39fbe7ff8424687f3c80febb4020
fabrezi <fkschon@gmail.com>
2018-08-14 11:38:17 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 #difference b/w bubble vs insert: 1. the number of for - loops
+ #we start loop at second element (index 1) since the first item is already sorted
 def insert(A):
     for i in range(1, len(A)):
         temp = A[i]
-        j = i -1
+        j = i -1#the last item
         while (j > -1) & (temp < A[j]):
             A[j+1] = A[j]
             j = j -1
