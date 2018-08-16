3559079b24368159bc1d89d89d63f2fbdeddcf4d
fabrezi <fkschon@gmail.com>
2018-08-16 16:27:42 -0400

    Update insert.py

--- insert.py
+++ insert.py
@@ -2,11 +2,11 @@
  #we start loop at second element (index 1) since the first item is already sorted
 def insert(A):
     for j in range(1,len(A)):
-        key = A[j]
-        i = j -1
+        key = A[j]#current vlaue
+        i = j -1#previous value 
         while(i>-1) & (A[i] > key):
             A[i+1] = A[i]
-            i = i -1
+            i = i -1#next item
             A[i+1] = key
     return A
 
