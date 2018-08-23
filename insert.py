d3470cbe182433b928f36d7e0326f40e7142f262
fabrezi <fkschon@gmail.com>
2018-08-23 14:40:24 -0400

    Update insert.py

--- insert.py
+++ insert.py
@@ -1,15 +1,22 @@
 #difference b/w bubble vs insert: 1. the number of for - loops
  #we start loop at second element (index 1) since the first item is already sorted
+
+file = open("lo", "r")
+A = []
+for val in file.read().split():
+    A.append(int(val))
+    file.close()
+    y = len(A)
+
 def insert(A):
     for j in range(1,len(A)):
-        key = A[j]#current vlaue
-        i = j -1#previous value 
-        while(i>-1) & (A[i] > key):
+        key = A[j]
+        i = j-1
+        while (i>-1) and (A[i] > key):
             A[i+1] = A[i]
             A[i] = key
-         i = i-1
+            i = i-1
     return A
-
-A = [5,2,4,6,1,3]
 insert(A)
 print(A)
+print(y)
