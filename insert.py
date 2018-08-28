81da9c1997fee6b45000a2141ab0b6da5185221b
fabrezi <fkschon@gmail.com>
2018-08-28 18:42:56 -0400

    Update insert.py

--- insert.py
+++ insert.py
@@ -1,5 +1,7 @@
 #difference b/w bubble vs insert: 1. the number of for - loops
  #we start loop at second element (index 1) since the first item is already sorted
+ # file("name", "mode")
+ # mode: r, w, a
 
 file = open("lo", "r")
 A = []
