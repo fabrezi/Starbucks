2824bc8dfdf8c997f06997902b757b42e79457e7
fabrezi <fkschon@gmail.com>
2018-08-30 15:55:16 -0400

    Update quicksort.py

--- quicksort.py
+++ quicksort.py
@@ -1,27 +1,24 @@
 #simple implementation. requires to be fixed as it doesn't have stable sort.
 # partition is fixed. the end node. giving wrong output --> logic error
 
-def partition(A, p , r):
+def quicksort(A,p,r):
+   if (p < r):
+       q = partition(A,p,r)
+       quicksort(A,p,q-1)
+       quicksort(A,q+1,r)
+
+def partition(A,p,r):
     i = p - 1
-    x = A[r]
-    for j in range(p,r-1,1):
-        if (A[j] <= x):
-            i = i+1
+    pivot = A[r]
+    for j in range(p, r-1):
+        if A[j] <= pivot:
+            i = i +1
             A[i] = A[j]
             A[j] = A[i]
-    A[p] = A[r]
-    A[r] = A[p]
+    A[i+1] = A[r]
+    A[r] = A[i+1]
+    return i+1
 
-    return (i+1)
-
-def quicksort(A,p,r):
-    if (p < r):
-        ip = partition(A,p,r)
-        quicksort(A, p, ip-1)
-        quicksort(A, ip+1, r)
-
-
-
-A = [2,8,7,1]
-quicksort(A,1,len(A)-1)
+A = [10, 7, 8, 9, 1, 5]
+quicksort(A,0, len(A)-1)
 print(A)
