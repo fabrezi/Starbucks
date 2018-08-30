2471a29d62b4c5c148458b686ef89a4fa6e331c0
fabrezi <fkschon@gmail.com>
2018-08-30 13:06:47 -0400

    Create quicksort.py

---
+++ quicksort.py
@@ -0,0 +1,27 @@
+#simple implementation. requires to be fixed as it doesn't have stable sort.
+# partition is fixed. the end node.
+
+ef partition(A, p , r):
+    i = p - 1
+    x = A[r]
+    for j in range(p,r-1,1):
+        if (A[j] <= x):
+            i = i+1
+            A[i] = A[j]
+            A[j] = A[i]
+    A[p] = A[r]
+    A[r] = A[p]
+
+    return (i+1)
+
+def quicksort(A,p,r):
+    if (p < r):
+        ip = partition(A,p,r)
+        quicksort(A, p, ip-1)
+        quicksort(A, ip+1, r)
+
+
+
+A = [2,8,7,1]
+quicksort(A,1,len(A)-1)
+print(A)
