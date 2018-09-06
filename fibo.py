3d674fb0a4118c8a91de4506fa50a53259461258
fabrezi <fkschon@gmail.com>
2018-09-06 15:07:03 -0400

    Update fibo.py

--- fibo.py
+++ fibo.py
@@ -1,17 +1,24 @@
-#works but get runtime errors at larger values. I should try the memoization technique
+#code: Siraj Raval
+# tabulation (bottom-up) approach
 
 
 def fib(n):
-    if (n < 0):
-        print("Error beta")
-    elif n <= 2:
-        return 1
-    else:
-        return fib(n - 1) + fib(n - 2)
 
+    # array declaration
+    f = [0]*(n+1)
 
+    # base case assignment
+    f[0] = f[1] = 1
 
+    # calculating the fibonacci and storing the values
+    for i in range(2 , n+1):
+        f[i] = f[i-1] + f[i-2]
+    return f[n]
+
+#Driver program to test the above function
 def main():
-    n = int(input("Enter value beta: "))
-    print(fib(n))
-main()
+    n = 25
+    print ("Fibonacci number is " , fib(n))
+
+if __name__=="__main__":
+    main()
