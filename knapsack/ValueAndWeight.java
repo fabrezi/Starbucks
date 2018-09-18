637ca4dd6ae30ce72cbcdc304e4167bf052ae9f9
fabrezi <fkschon@gmail.com>
2018-09-18 18:25:23 -0400

    Add files via upload

---
+++ knapsack/ValueAndWeight.java
@@ -0,0 +1,54 @@
+public class ValueAndWeight {
+    public int getWeight() {
+
+        return weight;
+
+    }
+
+
+
+    public ValueAndWeight(int weight, int value) {
+
+        this.weight = weight;
+
+        this.value = value;
+
+    }
+
+
+
+    private int weight;
+
+
+
+    public int getValue() {
+
+        return value;
+
+    }
+
+
+
+
+
+    @Override
+
+    public boolean equals(Object obj) {
+
+        if(obj instanceof ValueAndWeight){
+
+            ValueAndWeight that = (ValueAndWeight) obj;
+
+            return this.value == that.value && this.weight == that.weight;
+
+        }
+
+        return false;
+
+    }
+
+
+
+    private int value;
+
+}
