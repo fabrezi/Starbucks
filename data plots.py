9d8eef47d2c83d92d07a6ed89b349c7ae879c777
Farid Khan <fkschon@gmail.com>
2019-01-25 14:58:50 -0500

    Update data plots.py

--- data plots.py
+++ data plots.py
@@ -8,6 +8,7 @@ data['AGE'].hist()
 plt.show()
 
 #Make a scatter plot that shows the correlation between “AGE” and “OS_MONTHS”
+#this method is incomplete
 data = pd.read_csv('C:\\Users\\farid-PC\\Desktop\\class\\CS7265_BIG_DATA\\hw1\\Book1.CSV')
 #data.loc[data['OS_STATUS'] == "DECEASED", "AGE"].value_counts().sort_index()
 fig, ax = plt.subplots()
