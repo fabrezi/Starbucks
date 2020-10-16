1f7c53ca747fd16dd17247616102093a00eb08f0
fabrezi <fkschon@gmail.com>
2020-10-15 23:22:12 -0400

    Create warmup01.py

---
+++ warmup01.py
@@ -0,0 +1,17 @@
+#hello, 5 time, loop
+#area of circle
+
+'''
+i = 1
+while (i < 6):
+    print("hello world")
+    i += 1
+'''
+
+'''
+import math
+
+r = float(input("enter the radius of the circle:"))
+area = str(math.pi * r*r)
+print("the area of the circle:" + area)
+'''
