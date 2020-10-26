edb1d0f9242c578311823229342ae232b7789fa6
fabrezi <fkschon@gmail.com>
2020-10-26 18:05:07 -0400

    Update factorial.java

--- java-stuff/factorial.java
+++ java-stuff/factorial.java
@@ -12,3 +12,24 @@ public class minsk {
             return n * factorial(n-1);
     }
 }
+
+
+
+import java.util.Scanner;
+//add positive integers from n + (n-1) +...0
+public class hello {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("enter a digit:");
+        int n = input.nextInt();
+        System.out.println("the sum:" + zarb(n));
+    }
+    public static int zarb(int n){
+        if(n==0)
+            return 0;
+        else
+            return n + zarb(n-1);
+    }
+
+
+}
