7ef77f46c4470fbebe5b7c7922d47afb398c5bec
fabrezi <fkschon@gmail.com>
2020-10-25 22:24:50 -0400

    Update factorial.java

--- java-stuff/factorial.java
+++ java-stuff/factorial.java
@@ -1,1 +1,14 @@
 //calculate factorial through recursion calls
+public class minsk {
+    public static void main(String[] args) {
+        int n = 10;
+
+        System.out.println("the factorial is:" + factorial(n));
+    }
+    public static long factorial(int n){
+        if(n==0)
+            return 1;
+        else
+            return n * factorial(n-1);
+    }
+}
