0d543b3d6344ac0c05446a223635b0aa549d0819
fabrezi <fkschon@gmail.com>
2021-05-24 16:47:58 -0400

    Update fibo.java

--- java-stuff/fibo.java
+++ java-stuff/fibo.java
@@ -17,3 +17,24 @@ public class eu01 {
         System.out.println(fibo(n));
     }
 
+    
+ //generate list of fibo to n
+// n is static
+
+public class fibo {
+
+    public static void main(String[] args){
+        int i, n, t1 = 0;
+        int t2 = 1;
+        int next_term = t1 + t2;
+        int number_of_terms = 50;
+
+        for (i=1; i <= number_of_terms; i++){
+            System.out.println( next_term);
+            t1 = t2;
+            t2 = next_term;
+            next_term = t1 +  t2;
+        }
+    }
+
+}
