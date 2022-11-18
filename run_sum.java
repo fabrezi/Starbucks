99c313c708ee61a5965015bb7366d332a010476f
fabrezi <fkschon@gmail.com>
2022-11-18 14:52:42 -0500

    incomplete code

---
+++ run_sum.java
@@ -0,0 +1,19 @@
+//public class run_sum {
+//
+//    public static int run(){
+//        int [] arr = new int[4];
+//        arr[0] = num[0];
+//        for(int i=0; i < arr.length; i++){
+//
+//        }
+//
+//        return 0;
+//    }
+//
+//    public static void main(String[] args){
+//        arr = [1,2,3,4];
+//
+//    }
+//
+//
+//}
